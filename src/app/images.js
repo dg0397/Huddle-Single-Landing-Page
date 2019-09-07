12b934c535591bab77e2888d90357f039a84f5f7
dg0397 <rgg0397@gmail.com>
2019-09-07 13:28:39 -0400

    first commit

---
+++ src/app/images.js
@@ -0,0 +1,7 @@
+export function importImages(){
+    require("../static/images/favicon-32x32.png");
+    require("../static/images/bg-desktop.svg");
+    require("../static/images/bg-mobile.svg");
+    require("../static/images/illustration-mockups.svg");
+    require("../static/images/logo.svg");
+}
